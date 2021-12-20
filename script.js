57d9a672f5b33ed0cea45c00ccf21cd0cd921caa
Rithuraj Nambiar <69073735+rithurajnambiar17@users.noreply.github.com>
2021-12-20 21:06:09 +0530

    Updated script.js

--- script.js
+++ script.js
@@ -32,7 +32,7 @@ var coll = document.getElementsByClassName("collapsible");
 var i;
 
 for (i = 0; i < coll.length; i++){
-  coll[i].addEventListener("mouseover", function() {
+  coll[i].addEventListener("click", function() {
     this.classList.toggle("active");
     var content = this.nextElementSibling;
     if (content.style.maxHeight){
