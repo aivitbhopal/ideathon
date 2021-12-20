70cafe01af978680a32c4081844893aa3f4ed4e1
Rithuraj Nambiar <69073735+rithurajnambiar17@users.noreply.github.com>
2021-12-20 21:05:15 +0530

    Added FAQ Section

--- script.js
+++ script.js
@@ -26,3 +26,23 @@ if (distance < 0) {
     document.getElementById("clock").innerHTML = "Registrations Closed!";
 }
 }, 1000);
+
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++){
+  coll[i].addEventListener("mouseover", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+      content.style.visibility = "hidden";
+      content.style.margin = "0 2%";
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+      content.style.visibility = "visible";
+      content.style.margin = "12px 2%";
+    }
+  });
+}
