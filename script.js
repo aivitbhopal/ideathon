f49296e15a656c04edc5d6319c707d7e52e79d44
Kumar Prakhar <kumarprkhr@gmail.com>
2021-12-27 00:22:38 +0530

    report

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ var x = setInterval(function () {
   // If the count down is finished, write some text
   if (distance < 0) {
     clearInterval(x);
-    document.getElementById("clock").innerHTML = "Registrations Closed!";
+    document.getElementById("clock").innerHTML = "Submissions Closed!";
   }
 }, 1000);
 
